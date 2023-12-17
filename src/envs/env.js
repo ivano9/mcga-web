4753b94e6c9d2f6f05f80a0ed576f70c75578012
Ivan Giovanazzi <ivanog@pm.me>
2023-12-17 15:12:24 -0300

    changed url api

--- src/envs/env.js
+++ src/envs/env.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    api: 'https://mcga-orders.onrender.com/api/',
+    api: 'https://mcga-orders.onrender.com/api/v1.0',
     tokenRefresh: 300,
     client_id: 4,
     client_secret: '',
